28218de93f2c92b5b436b84ed5834534a54de84f
ouyang <stwflyfox@163.com>
2025-03-17 16:58:33 +0800

    更新官方版本、TinyMce增加配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,10 +22,11 @@ module.exports = {
   plugins: ['vue'],
   rules: {
     'no-console': 0,
-    'no-debugger': 0,
+    'no-': 0,
     'space-before-function-paren': 'off',
     'vue/multi-word-component-names': 'off',
-    'no-use-before-define': 'error',
-    'no-unused-vars': ['error', { ignoreRestSiblings: true, varsIgnorePattern: '^_', argsIgnorePattern: '^_' }]
+    'prefer-const': 'warn', // 将 'prefer-const' 规则从 'error' 改为 'warning'
+    'no-use-before-define': 'warn',
+    'no-unused-vars': ['warn', { ignoreRestSiblings: true, varsIgnorePattern: '^_', argsIgnorePattern: '^_' }]
   }
 }
