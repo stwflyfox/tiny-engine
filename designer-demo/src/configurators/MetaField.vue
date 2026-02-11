7b47f789cf1f6b30138f95a9f32e7fc6beab968f
ouyang <stwflyfox@163.com>
2026-02-11 14:54:31 +0800

    更新图表配置

--- designer-demo/src/configurators/MetaField.vue
+++ designer-demo/src/configurators/MetaField.vue
@@ -1,12 +1,22 @@
 <template>
   <tiny-input v-model="state.selected"></tiny-input>
-
+  <tiny-base-select        
+          v-model="state.tableName" 
+          :searchable="true"
+          placeholder="选择业务表"
+          @change="changeTable"
+          clearable
+          :options="state.tableList"
+        >
+        
+        </tiny-base-select>
   <tiny-base-select
     v-model="state.selected"
     :multiple="multi"
     :is-drop-inherit-width="true"
     :show-alloption="false"
     :clearable="true"
+    placeholder="选择字段"
     @change="handleChange"
     :options="state.options"
   >
@@ -18,11 +28,14 @@ import { reactive, watchEffect } from 'vue'
 import { Select, Input } from '@opentiny/vue'
 import i18n from '@opentiny/tiny-engine-common/js/i18n'
 
+import { Page } from '@opentiny/tiny-engine'
+
 import { request, METHOD } from './request'
 
 export default {
   components: {
     TinyBaseSelect: Select,
+    TinyOption :Select.Option,
     TinyInput: Input
   },
   props: {
@@ -48,11 +61,32 @@ export default {
     const { locale } = i18n.global
     const state = reactive({
       selected: props.modelValue && props.modelValue.value ? props.modelValue.value : '',
+      tableName: '',
       options: [],
+      tableList : [],
       defaultParameters: [],
       selectedDefault: '',
       defaultValue: ''
     })
+
+
+
+    const changeTable = (value) => {
+       getTableFields(value);
+    }
+
+    const getTableFields = (TableName) => {
+      request('/System/GetTableFieldsByName', METHOD.POST, { TableName: TableName }).then((result) => {
+        state.options = []
+
+        result.forEach((p) => {
+          state.options.push({
+            label: p.field_common + '(' + p.field_name + ')',
+            value: 'this.state.formData.' + p.field_name
+          })
+        })
+      })
+    }
 
     const handleChange = (arg) => {
       emit('update:modelValue', {
@@ -67,36 +101,31 @@ export default {
 
     return {
       state,
-      locale,
+      locale,     
+      changeTable, 
+      getTableFields,
       handleChange
     }
   },
   mounted() {
-    var TableName = sessionStorage.getItem('TableName')
 
-    if (TableName != null && TableName != '') {
-      request('/System/GetTableFieldsByName', METHOD.POST, { TableName: TableName }).then((result) => {
-        this.state.options = []
 
-        result.forEach((p) => {
-          this.state.options.push({
-            label: p.field_common + '(' + p.field_name + ')',
-            value: 'this.state.formData.' + p.field_name
-          })
-        })
+
+    request('/System/QueryTableList', METHOD.POST).then((result) => {
+      
+      this.state.tableList = []
+       result.forEach(item=>{        
+        this.state.tableList.push({
+          key : item.table_name,
+          label : item.table_common + '(' + item.table_name + ')',
+          value : item.table_name,
+        });
       })
-    }
+    })
+
   }
 }
 </script>
 <style scoped>
-.tiny-select-dropdown__item {
-  padding: 0 4px;
-  width: 100%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  display: inline-block;
-  line-height: 30px;
-}
+
 </style>
