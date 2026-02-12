50cd5624e1a1a202b972096c0242787b633ea228
ouyang <stwflyfox@163.com>
2026-02-12 10:28:20 +0800

    增加编译@opentiny/tiny-engine-utils

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,5 +1,5 @@
 const { exec } = require('node:child_process')
 
 exec(
-  'pnpm -F @opentiny/tiny-engine-vite-plugin-meta-comments -F @opentiny/tiny-engine-common -F @opentiny/tiny-engine-dsl-vue build'
+  'pnpm -F @opentiny/tiny-engine-vite-plugin-meta-comments -F @opentiny/tiny-engine-common -F @opentiny/tiny-engine-utils -F @opentiny/tiny-engine-dsl-vue build'
 )
